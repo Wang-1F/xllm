dafaf555ae58ae26a3db3f562c55a9c0f670e11e
Kang-Meng <kangmeng3@jd.com>
2026-01-19 11:50:53 +0800

    bugfix: fix cudagraph capture error with compute_next_round_input_async.

--- xllm/core/runtime/concurrent_rec_worker_impl.cpp
+++ xllm/core/runtime/concurrent_rec_worker_impl.cpp
@@ -170,9 +170,8 @@ ConcurrentRecWorkerImpl::step_async(const ForwardInput& input) {
   folly::Promise<std::optional<ForwardOutput>> promise;
   auto future = promise.getSemiFuture();
 
-  if (FLAGS_enable_graph) {
-    warmup(input);
-  }
+  warmup(input);
+
   // Use schedule() to assign tasks, letting ThreadPool automatically select
   // idle threads The logic for allocating instance_id happens when the task
   // executes (see lambda below)
@@ -343,6 +342,10 @@ ConcurrentRecWorkerImpl::ConcurrentLlmRecPureDevicePipeline::step(
   std::optional<folly::SemiFuture<NextRoundInputResults>>
       next_round_async_result;
 
+  bool is_warmup = concurrent_worker_.warmup_set_.find(
+                       mutable_input.input_params.num_sequences) ==
+                   concurrent_worker_.warmup_set_.end();
+
   for (int32_t round = 0; round < total_rounds; ++round) {
     const auto& sampling_params = round > 0
                                       ? mutable_input.decoder_sampling_params
@@ -353,8 +356,7 @@ ConcurrentRecWorkerImpl::ConcurrentLlmRecPureDevicePipeline::step(
 
     // Start async computation for next round input (overlap with GPU
     // logits/sampling)
-    // TODO: support async computation for next round input
-    if (round < total_rounds - 1 && !FLAGS_enable_graph) {
+    if (round < total_rounds - 1 && !is_warmup) {
       next_round_async_result =
           compute_next_round_input_async(mutable_input.input_params.kv_seq_lens,
                                          round,
